b7464548d1fb788ba90c527a620597ab8c802b2e
Pasquale Catapano <pasquale@catapano.me>
2024-01-04 21:38:27 +0100

    test install docker

--- docker.sh
+++ docker.sh
@@ -1,3 +1,6 @@
+sudo wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/docker-ce_24.0.7-1~debian.12~bookworm_amd64.deb"
+sudo dpkg -i docker-ce_24.0.7-1~debian.12~bookworm_amd64.deb
+
 sudo wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/containerd.io_1.6.26-1_amd64.deb"
 sudo dpkg -i containerd.io_1.6.26-1_amd64.deb
 
@@ -9,9 +12,6 @@ sudo dpkg -i docker-ce-cli_24.0.7-1~debian.12~bookworm_amd64.deb
 
 sudo wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/docker-ce-rootless-extras_24.0.7-1~debian.12~bookworm_amd64.deb"
 sudo dpkg -i docker-ce-rootless-extras_24.0.7-1~debian.12~bookworm_amd64.deb
-
-sudo wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/docker-ce_24.0.7-1~debian.12~bookworm_amd64.deb"
-sudo dpkg -i docker-ce_24.0.7-1~debian.12~bookworm_amd64.deb
 
 sudo wget "https://download.docker.com/linux/debian/dists/bookworm/pool/stable/amd64/docker-compose-plugin_2.21.0-1~debian.12~bookworm_amd64.deb"
 sudo dpkg -i docker-compose-plugin_2.21.0-1~debian.12~bookworm_amd64.deb
