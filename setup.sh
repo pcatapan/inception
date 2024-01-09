601d3a13be476515ac4ab17fe19cd20359bbe239
Pasquale Catapano <pasquale@catapano.me>
2024-01-09 20:52:28 +0100

    fix

--- setup.sh
+++ setup.sh
@@ -12,20 +12,12 @@ BLANK="\e[0m"
 
 
 echo -e $PURPLE "Updating system..." $BLANK
-sudo apt update 2> /dev/null
+sudo apt-get update 2> /dev/null
 sudo apt upgrade -y 2> /dev/null
 echo -e $GREEN "Done." $BLANK
 
 echo -e $PURPLE "Installing git, make and vim..."
-sudo apt install -y git make vim 2> /dev/null
-echo -e $GREEN "Done." $BLANK
-
-echo -e $PURPLE "Installing vscode..."
-sudo snap install --classic code 2> /dev/null
-echo -e $GREEN "Done." $BLANK
-
-echo -e $PURPLE "Installing filezilla..."
-sudo snap install --edge filezilla 2> /dev/null
+sudo apt install -y make vim 2> /dev/null
 echo -e $GREEN "Done." $BLANK
 
 if grep -q $DOMAIN /etc/hosts; then
@@ -35,5 +27,20 @@ if grep -q $DOMAIN /etc/hosts; then
 fi
 
 echo -e $PURPLE "Installing docker dependencies..."
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-echo -e $GREEN "Done."
+sudo apt install -y install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+echo -e $GREEN "Done." $BLANK
+
+echo -e $PURPLE "Installing docker..."
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo -e $GREEN "Done." $BLANK
+
+echo -e $PURPLE "Re start" $BLANK
+sudo reboot
